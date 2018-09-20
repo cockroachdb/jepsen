c7dda36da32391c5b4597a6c15cc5817ac08d13e
Ben Darnell <ben@bendarnell.com>
2018-09-20 12:23:55 -0400

    catch everything

--- jepsen/src/jepsen/core.clj
+++ jepsen/src/jepsen/core.clj
@@ -373,7 +373,7 @@
               (try
                 ; Open a new client
                 (set! (.client this) (client/open! (:client test) test node))
-                (catch RuntimeException e
+                (catch Throwable e
                   (warn e "Error opening client")
                   (let [fail (assoc op
                                     :type  :fail
