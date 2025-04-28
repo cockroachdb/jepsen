d3e8f55208fbc509d55684e4506daee9ad94e49f
Ludovic Leroux <ludo.leroux@cockroachlabs.com>
2025-04-28 17:51:32 -0400

    cockroachdb: bump to java17
    Jepsen tests timeout in the Analyzing step with OpenJDK-8 on the s390x
    architecture. This patch bumps the java build version to Java 17 and
    adds jaxb as a dependency as it was removed in Java 9.

--- cockroachdb/project.clj
+++ cockroachdb/project.clj
@@ -6,16 +6,11 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [jepsen "0.1.9-SNAPSHOT"]
                  [org.clojure/java.jdbc "0.6.1"]
-                 [org.postgresql/postgresql "9.4.1211"]]
-  :jvm-opts ["-Xmx12g"
-             "-XX:+UseConcMarkSweepGC"
-             "-XX:+UseParNewGC"
-             "-XX:+CMSParallelRemarkEnabled"
-             "-XX:+AggressiveOpts"
-             "-XX:+UseFastAccessorMethods"
-             "-XX:MaxInlineLevel=32"
-             "-XX:MaxRecursiveInlineLevel=2"
-             "-server"]
+                 [org.postgresql/postgresql "9.4.1211"]
+                 [jakarta.xml.bind/jakarta.xml.bind-api "2.3.3"]
+                 [org.glassfish.jaxb/jaxb-runtime "2.3.3"]]
+  :javac-options ["--release" "17"]
+  :jvm-opts ["-Xmx12g"]
   :main jepsen.cockroach.runner
   :aot [jepsen.cockroach.runner
         clojure.tools.logging.impl])
